6451ef7d16c57ff7542b4a830164b8172ef75841
GABRIEL MAR DANTAS <191121232@acad.dvuadmin.net>
2019-08-30 20:55:11 -0400

    Implementando uma classe, seus atributos e métodos.Criando objeto a partir da classe instanciada

---
+++ src/Cachorro.java
@@ -0,0 +1,36 @@
+/* Disciplina: Programação Orientada a Objetos
+ * Prof.: Orlewilson B. Maia 
+ * Autor: Gabriel Mar Dantas
+ * Data: 30/08/2019
+ * Descrição: Primeiro programa em Java
+ * 
+ * 
+ * 
+ * */
+
+
+
+
+
+public class Cachorro {
+	
+	//atributos
+	String raca;
+	String cor;
+	float comprimento;
+	float peso;
+	String apelido;
+	
+	
+	//métodos
+	void latir(){
+		System.out.println("Au, au!!");
+	}; 
+	void correr(){
+		System.out.println("Correndo...");
+	};
+	void morder(){
+		System.out.println("Mordendo...");
+	};
+
+}
--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,28 @@
  * */
 
 // Classe principal para executar o projeto
-public class Main {
+public class Main {	
 	// método principal para rodar código
 	public static void main(String[]args){
-		//mostra uma mensagem no console
-		System.out.println("Hello World!!");
+		//criar objetos
+		Cachorro toto = new Cachorro();
+		//alterando conteúdo dos atributos
+		toto.apelido = "toto";
+		toto.comprimento = 1.2f;
+		toto.cor = "amarelo";
+		toto.peso = 20f;
+		toto.raca = "spitz alemão";
+		
+		//mostrar conteúdos dos atributos
+		System.out.println("Apelido:" + toto.apelido);
+		System.out.println("Raça:" + toto.raca);
+		System.out.println("Cor:" + toto.cor);
+		System.out.println("Peso:" + toto.peso);
+		System.out.println("Comprimento:" + toto.comprimento);
+		
+		//chamando os métodos
+		toto.correr();
+		toto.morder();
+		toto.latir();
 	}
 	}
